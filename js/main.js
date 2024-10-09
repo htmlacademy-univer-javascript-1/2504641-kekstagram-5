9f2018cf2492f69f8474ffa804417f7d01ba187c
alenakrash <alenakrash@yandex.ru>
2024-10-09 22:39:27 +0500

    Дз 4 модуль

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ const MESSAGES = [
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
 ];
 
-const NAMES =[
+const NAMES = [
   'Наталья',
   'Анастасия',
   'Елена',
@@ -25,20 +25,20 @@ const NAMES =[
   'Полина',
 ];
 
-const DESCRIPTIONS =[
-  "Солнечное утро в парке с моим любимым кофе",
-  "Восхитительный закат на берегу моря" ,
-  "Веселый пикник с друзьями в лесу",
-  "Мой любимый кот спит на подоконнике",
-  "Вкусный ужин в ресторане с видом на город",
-  "Прогулка по старинным улочкам старого города",
-  "Моя новая книга и чашка горячего чая",
-  "Веселая вечеринка с друзьями в баре",
-  "Моя новая татуировка - память о путешествии",
-  "Спортивная тренировка на свежем воздухе",
-  "Моя любимая роза в саду",
-  "Вкусный завтрак в постели", 
-  "Моя новая коллекция растений",
+const DESCRIPTIONS = [
+  'Солнечное утро в парке с моим любимым кофе',
+  'Восхитительный закат на берегу моря' ,
+  'Веселый пикник с друзьями в лесу',
+  'Мой любимый кот спит на подоконнике',
+  'Вкусный ужин в ресторане с видом на город',
+  'Прогулка по старинным улочкам старого города',
+  'Моя новая книга и чашка горячего чая',
+  'Веселая вечеринка с друзьями в баре',
+  'Моя новая татуировка - память о путешествии',
+  'Спортивная тренировка на свежем воздухе',
+  'Моя любимая роза в саду',
+  'Вкусный завтрак в постели',
+  'Моя новая коллекция растений',
 ];
 
 const getRandomInteger = (a, b) => {
@@ -50,11 +50,11 @@ const getRandomInteger = (a, b) => {
 const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
 
 function generateIdFuncCom () {
-  let lastGeneratedId=0;
+  let lastGeneratedId = 0;
 
   return function (){
-    lastGeneratedId+=1;
-    return lastGeneratedId
+    lastGeneratedId += 1;
+    return lastGeneratedId;
   };
 }
 
@@ -62,7 +62,7 @@ const generateId = generateIdFuncCom();
 
 const createComment = () => ({
   id: generateId,
-  avatar: 'img/avatar-{getRandomInteger(1,6)}.svg',
+  avatar: `img/avatar-${getRandomInteger(1,6)}.svg`,
   message: getRandomArrayElement(MESSAGES),
   name: getRandomArrayElement(NAMES),
 });
@@ -74,10 +74,6 @@ function createRandomIdFromRangeGenerator (min, max) {
 
   return function () {
     let currentValue = getRandomInteger(min, max);
-    if (previousValues.length >= (max - min + 1)) {
-      console.error('Перебраны все числа из диапазона от ' + min + ' до ' + max);
-      return null;
-    }
     while (previousValues.includes(currentValue)) {
       currentValue = getRandomInteger(min, max);
     }
@@ -90,14 +86,13 @@ const createPhoto = function() {
   const idGenerator = createRandomIdFromRangeGenerator(1,25);
   const urlGenerator = createRandomIdFromRangeGenerator(1,25);
   return{
-  id: idGenerator(),
-  url: `photos/${urlGenerator()}.jpg`,
-  desription: getRandomArrayElement(DESCRIPTIONS),
-  likes: getRandomInteger(15,200),
-  comments: comments
+    id: idGenerator(),
+    url: `photos/${urlGenerator()}.jpg`,
+    desription: getRandomArrayElement(DESCRIPTIONS),
+    likes: getRandomInteger(15,200),
+    comments: comments
   };
 };
 
 const photos = Array.from({length: 25}, createPhoto);
-console.log(photos)
-  
+console.log(photos);
